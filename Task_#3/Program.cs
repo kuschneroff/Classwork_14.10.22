8dc7bfb97236b4f470d7b19a66c4e4799a1171d2
Aleksey Kushnerov <kuschneroff.alex@yandex.ru>
2022-10-15 23:39:11 +0300

    Задача 33: Задайте массив. напишите программу, которая опредедяет присутствует ли заданное число в массиве.

---
+++ Task_#3/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Задача 33: Задайте массив. напишите программу, 
+которая опредедяет присутствует ли заданное число в массиве 
+*/
+
+
+    void PrintArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Введите число для сравнения");
+int numberX = int.Parse(Console.ReadLine() ??"");
+
+int [] RandomNum ()
+{
+int [] ResultArray = new int [10];
+Random rnd = new Random();
+for (int i = 0; i < ResultArray.Length; i++)
+{
+    ResultArray [i] = rnd.Next(-10, 10);
+}
+return ResultArray;
+}
+
+void Compare(int [] array, int numberX)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] == numberX)
+        {
+            Console.WriteLine($"Введенное число {numberX} есть в массиве");
+            return;
+        }
+    }
+    Console.WriteLine("Такого числа нет");
+}
+
+int[] arr = RandomNum();
+PrintArray(arr);
+Compare(arr, numberX);
